a72935a87b3c62ccfb5a4310e1703c364f625c98
wenqu <1663850451@qq.com>
2019-03-18 01:53:49 +0800

    init files

---
+++ src/main/java/com/star/eden/EdenApplication.java
@@ -0,0 +1,13 @@
+package com.star.eden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdenApplication.class, args);
+    }
+
+}
