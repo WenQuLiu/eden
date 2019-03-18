9924b272d16737fe7234299531c76e4d1b0e942e
wenqu <1663850451@qq.com>
2019-03-18 10:42:26 +0800

    提交

--- src/main/java/com/star/eden/EdenApplication.java
+++ src/main/java/com/star/eden/EdenApplication.java
@@ -2,8 +2,11 @@ package com.star.eden;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+//@SpringBootApplication
+//@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 public class EdenApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/star/eden/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.star.eden.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+    @RequestMapping("a")
+    public String aTest(){
+        return "Hello World";
+    }
+}
